f6f8ba7ee648310e3d9b2e86a34830d7556d78b4
Niki <niki.manoledaki@gmail.com>
2020-03-19 14:38:25 +0000

    Returns misspelled word surrounded by dash

--- spec/spellchecker_spec.rb
+++ spec/spellchecker_spec.rb
@@ -6,6 +6,11 @@ describe Spellchecker do
             string = "Hello world"
             expect(subject.spellcheck(string)).to eq "Hello world"
         end
+
+        it 'returns misspelled word surrounded by dash' do
+            string = "Hello wurld"
+            expect(subject.spellcheck(string)).to eq "Hello -wurld-"
+        end
     end
 
     describe '#word_bank' do
