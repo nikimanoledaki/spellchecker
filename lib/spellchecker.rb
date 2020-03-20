a9fd58f60292bd4aeed985e231135e7fc26dad25
Niki <18622989+nikimanoledaki@users.noreply.github.com>
2020-03-20 16:32:13 +0000

    Refactors spellcheck method

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,16 +1,26 @@
 class Spellchecker
+    attr_reader :result
+
+    def initialize
+        @result = []
+    end
 
     def spellcheck(string)
-        result = []
         words = string.split(' ')
-        words.each do |word|
-           if word_bank.include?(word)
-            result.append(word)
-           else 
-            result.append("-#{word}-")
-           end
+        words.each { |word| word_bank?(word) }
+        display
+    end
+
+    def word_bank?(word)
+        if word_bank.include?(word)
+            @result.append(word)
+        else 
+            @result.append("-#{word}-")
         end
-        result.join(' ')
+    end
+
+    def display
+        @result.join(' ')
     end
 
     def word_bank
